707e7d6c0561e8e3951787e02cad478ec71d2a7a
Kender Elford <Kender.Elford@nike.com>
2015-02-13 13:41:56 -0800

    finished application serializer

--- src/main/scala/io/github/kender/spray/eureka/client/EurekaSerialization.scala
+++ src/main/scala/io/github/kender/spray/eureka/client/EurekaSerialization.scala
@@ -1,6 +1,7 @@
 package io.github.kender.spray.eureka.client
 
 import io.github.kender.spray.eureka.{Application, InstanceInfo}
+import org.json4s.Extraction.decompose
 import org.json4s.JsonAST._
 import org.json4s.jackson.Serialization
 import org.json4s.{CustomSerializer, NoTypeHints}
@@ -10,24 +11,24 @@ object EurekaSerialization {
   class ApplicationSerializer extends CustomSerializer[Application](implicit format => ( {
     case JObject(JField("name", JString(name)) :: Nil) =>
       Application(name, Nil)
+
     case JObject(JField("name", JString(name)) :: JField("instance", instance: JObject) :: Nil) =>
       Application(name, List(instance.extract[InstanceInfo]))
+
     case JObject(JField("name", JString(name)) :: JField("instance", JArray(instances)) :: Nil) =>
       Application(name, instances.map(_.extract[InstanceInfo]))
 
   }, {
     case application: Application =>
       JObject(
-        "name" -> JString(application.name)
+        "name" -> decompose(application.name),
+        "instance" -> decompose(application.instances)
       )
   }))
 
-
   object Implicits {
-
     implicit val eurekaFormats =
       Serialization.formats(NoTypeHints) +
       new EurekaSerialization.ApplicationSerializer
   }
-
 }
